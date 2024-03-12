ccf8b3da4b756e8b74ab94de832b61fb4306b774
elena-dogman <dogman.choli@gmail.com>
2024-03-12 19:05:07 +0300

    feat: solve formatDate problem

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -149,7 +149,28 @@ function isDateInPeriod(date, period) {
 }
 
 /**
+ * Returns the date formatted in 'M/D/YYYY, hh:mm:ss a'.
  *
+ * @param {string} date - The date to be formatted, in ISO 8601 format (e.g., 'YYYY-MM-DDTHH:mm:ss.sssZ').
+ * @return {string} - The date formatted in 'Month/Day/Year, Hour:Minute:Second AM/PM'.
+ *
+ * @example:
+ * '2024-02-01T15:00:00.000Z' => '2/1/2024, 3:00:00 PM'
+ * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
+ * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
+ */
+function formatDate(date) {
+  const formattedDate = new Date(date);
+
+  const month = formattedDate.getMonth() + 1;
+  const day = formattedDate.getDate();
+  const year = formattedDate.getFullYear();
+  const hours = String(formattedDate.getHours()).padStart(2, '0');
+  const minutes = String(formattedDate.getMinutes()).padStart(2, '0');
+  const seconds = String(formattedDate.getSeconds()).padStart(2, '0');
+  const meridiem = formattedDate.getHours() < 12 ? 'AM' : 'PM';
+
+  return `${month}/${day}/${year}, ${hours}:${minutes}:${seconds} ${meridiem}`;
 }
 
 /**
